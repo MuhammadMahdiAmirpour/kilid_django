fc75713ef55577381471982e139868b4854a09ab
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2024-01-28 22:32:18 +0330

    made the migrations and registered the models in the admin.py file

--- kilid_django/kilid_app/admin.py
+++ kilid_django/kilid_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from kilid_app.models import Province, Region, City, AgencyManager, EstateAgency, AgencyAdvisor, House, Advertisement
 
 # Register your models here.
+admin.site.register(Province)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(AgencyAdvisor)
+admin.site.register(EstateAgency)
+admin.site.register(AgencyManager)
+admin.site.register(House)
+admin.site.register(Advertisement)
